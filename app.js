1874f3fdac321ff9028fc40934164e5d2898c3da
Jeevan HR <jeevanhr7@gmail.com>
2016-01-12 21:02:02 +0530

    addpost funtion and form added

--- app.js
+++ app.js
@@ -1,13 +1,6 @@
-angular.module('mainApp', []).factory('$exceptionHandler', function() {
-    return function(exception, cause) {
-        exception.message += ' (caused by "' + cause + '")';
-        throw exception;
-    };
-});
-
-
 var mainapp = angular.module('mainApp', []);
 mainapp.controller('control', function ($scope) {
+
     $scope.posts = [
         {title: 'post 1', upvotes: 5},
         {title: 'post 2', upvotes: 2},
@@ -15,15 +8,12 @@ mainapp.controller('control', function ($scope) {
         {title: 'post 4', upvotes: 9},
         {title: 'post 5', upvotes: 4}
     ];
-
-    $scope.addpost=function()
-    {
-        $scope.posts.push({'title':'A new Post','upvotes':0})
+    $scope.addpost = function () {
+        $scope.upvotes=+1;
+        $scope.posts.push({'title': $scope.title, 'upvotes': $scope.upvotes});
+        $scope.title = '';
     }
 
-    $scope.removepost=function()
-    {
-        $scope.posts.shift();
-    }
+
 
 });
